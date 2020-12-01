c6b2df294218eb38d50507a1790b269573525ac1
SStaili <staili.sofiane.1996@gmail.com>
2020-12-01 10:58:31 +0100

    Clarification et ajouts
    J'ai ajouter des caracteristiques nécessaires manquantes aux futures combats et ajouter la classe monstre.

---
+++ assets/js/monstre.js
@@ -0,0 +1,15 @@
+// Generateur de monstre - Sofiane
+ export class Monstre {
+    constructor(nom, pv, defense, atk, vitesse) {
+        this.nom = nom;
+        this.pv = pv;
+        this.defense = defense;
+        this.atk = atk;
+        this.vitesse = vitesse
+    }
+  }
+  //Ajouter un exemple de monstre - Sofiane
+
+
+
+  export default Monstre;
