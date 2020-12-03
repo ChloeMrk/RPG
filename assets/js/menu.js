46dbc83513e5ede5c552fa33ffa47ba562fad3ab
ChloeMrk <56391670+ChloeMrk@users.noreply.github.com>
2020-12-03 01:23:08 +0100

    Modif Menu
    Ajouter des image pour faire jolie

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,8 @@
 function myFunction() {
   var popup = document.getElementById("myPopup");
   var popup1 = document.getElementById("myPopup1");
+  var popup2 = document.getElementById("myPopup2");
   popup.classList.toggle("show");
   popup1.classList.toggle("show");
+  popup2.classList.toggle("show");
 }
